3e3a02c856afade3a2a654376f48575ba9c18658
Vincent Lobrigo <vlobrigo@yahoo.com>
2017-01-04 17:59:06 +0800

    UG Google Autocomplete/ Suggest component implementation, not yet applied to pages

---
+++ app/containers/UGGoogleSuggest/PlacesSuggest.js
@@ -0,0 +1,15 @@
+
+// ************************************************************************
+// Google Suggest component - Vinz 2016-12-22
+// styled component/container for suggestions
+// this feature is still on development by ber
+// ************************************************************************
+
+import styled from 'styled-components';
+
+const PlacesSuggest = styled.div` 
+  width: 100%;
+  position: relative; 
+`;
+export default PlacesSuggest;
+
